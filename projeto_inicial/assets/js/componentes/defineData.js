23e287ed5f37b81338549b4e0a66fc89baf9e3d5
Lancelotti-beta <brunojosemarques.pe@gmail.com>
2022-07-04 23:02:59 -0300

    iniciando - Parte II | Adicionando momentTime
    aprendendo a usar libs no projeto, refatorando código e adicionado input
    te data no projeto - rotomando o projeto do Ceep

---
+++ projeto_inicial/assets/js/componentes/defineData.js
@@ -0,0 +1,12 @@
+// function DataEHora(data) {
+//     //moment().format('l')
+//     const valorDoInputDate = moment(data)
+//     const dataDoEvento = document.createElement('span')
+//     dataDoEvento.classList.add('content')
+//     dataDoEvento.innerText = valorDoInputDate
+//     return dataDoEvento
+// }
+// export default DataEHora
+
+
+//Trabalho alterado, script trocado
--- projeto_inicial/assets/js/main.js
+++ projeto_inicial/assets/js/main.js
@@ -1,34 +1,41 @@
 import BotaoConcluir from "./componentes/concluirTarefa.js";
 import BotaoDeletar from "./componentes/deletarTarefa.js";
 
-const campoDeEscrita = document.querySelector('[data-form-input]')
-const botaoNovoItem = document.querySelector('[data-form-button]')
-
-botaoNovoItem.addEventListener('click', e => {
+function itemNaLista(e) {
     e.preventDefault()
-    adicionaNaLista(campoDeEscrita.value)
-})
-
-function adicionaNaLista(tarefa) {
+    const campoDeEscrita = document.querySelector('[data-form-input]')
+    const paragrafo = campoDeEscrita.value
     const lista = document.querySelector('[data-list]')
-    const item = tarefa
 
+    //Formatando a hora e data
+    const dataEHora = document.querySelector('[data-form-date]')
+    const pegandoData = moment(dataEHora.value)
+    const data = pegandoData.format('LL')
+
+    const valor = {
+        data,
+        paragrafo
+    }
+
+    //adicionando o valor do <p> ao <li>
+    lista.appendChild(adicionaNaLista(valor))
+    lista = " "
+}
+
+function adicionaNaLista( { data, paragrafo } ) {
     //criando uma <li>
     const topico = document.createElement('li')
     topico.classList.add('task')
-
     //adionando o valor do <input> ao <p>
-    const conteudo = `<p class="content">${item}</p>`
+    const conteudo = `<p class="content">${data} | ${paragrafo}</p>`
 
     topico.innerHTML = conteudo
-
     //adiciona botão a <li>
     topico.appendChild(BotaoConcluir())
     topico.appendChild(BotaoDeletar())
 
-    //adicionando o valor do <p> ao <li>
-    lista.appendChild(topico)
-
-    item = ""
+    return topico
 }
 
+const botaoNovoItem = document.querySelector('[data-form-button]')
+botaoNovoItem.addEventListener('click', itemNaLista)
